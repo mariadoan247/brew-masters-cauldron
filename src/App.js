329dbc61055654cb470e316d92cfbc7b2610c2f4
Rebekah <joeun514@gmail.com>
2023-09-18 01:36:51 -0500

    left nav is responsive now. Trying to fix the display though with more icons

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
 import React from 'react';
-import LoginPage from "./LoginPage"; // Import your login page component
+import LoginPage from "./LoginPage";
+import ContactPage from "./ContactPage";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSmile, faUserCircle, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 
 function App() {
@@ -12,37 +14,39 @@ function App() {
     <Router>
       <>
         <div className="topNav">
+        <div className="topNav-left">
+          <Link to="/contact" className="contact-link">
+              <FontAwesomeIcon icon={faEnvelope} size="2xl" style={{ color: "#ffffff" }} />
+            </Link>
+          </div>
           <div className="topNav-right"></div>
           <div className="link">
-            {/* Use the Link component to navigate to the login page */}
             <Link to="/login" className="text-wrapper">Log In</Link>
           </div>
         </div>
 
         {/* Left Navbar */}
-        <div className="div-side-nav-links">
-        <div className="box">
-          <div className="rectangle" />
-          
+        
+          <div className="leftNav" />
+          <div className="div-side-nav-links">
             <Nav>
               <Nav.Link className="nav-link">
-              <FontAwesomeIcon icon="fa-regular fa-face-smile" size="2xl" style={{color: "#ffffff",}} />
+              <FontAwesomeIcon icon={faSmile} size="2x" style={{ color: "#ffffff" }} />
                 <span>Classes</span>
               </Nav.Link>
               <Nav.Link className="nav-link">
-              <FontAwesomeIcon icon="fa-regular fa-face-smile" size="2xl" style={{color: "#ffffff",}} />
+              <FontAwesomeIcon icon={faUserCircle} size="2xl" style={{color: "#ffffff"}} />
                 <span>Races</span>
               </Nav.Link>
-              {/* Add similar Nav.Link elements for other items */}
             </Nav>
           </div>
-        </div>
+       
 
 
 
-        {/* Define a route to render the Login page */}
         <Routes>
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </>
     </Router>
