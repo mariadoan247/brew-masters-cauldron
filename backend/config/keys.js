6937f01dc6536bdd61aded15f2b65d4f928a3888
mariadoan247 <mariadoan247@ou.edu>
2023-10-02 15:26:32 -0500

    database configurations created

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mariadoan247:0AMWE58AVDUN5F1R@brewmasters-cauldron.knpgadz.mongodb.net/?retryWrites=true&w=majority",
+    apiKey: "p0OQjP17ejlY65NAGaytJpVEoIFeWhWa1ecUk6OfW4sEj9x6qDXhJ4IUodYjpS8B",
+    secretOrKey: "secret"
+  };
