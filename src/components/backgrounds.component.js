b42209a99d6d57d88ef0b3fbdc4152c8b71c56b9
Rebekah <joeun514@gmail.com>
2023-10-09 13:35:54 -0500

    the row names can now be navigated to a new page

--- src/components/backgrounds.component.js
+++ src/components/backgrounds.component.js
@@ -40,6 +40,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { Container, Stack, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+
 
 const drawerWidth = 240;
 
@@ -118,63 +121,70 @@ const SearchBoxContainer = styled("div")({
 
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
     'label + &': {
-      marginTop: theme.spacing(3),
+        marginTop: theme.spacing(3),
     },
     '& .MuiInputBase-input': {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.mode === 'light' ? '#F3F6F9' : '#1A2027',
-      border: '1px solid',
-      borderColor: theme.palette.mode === 'light' ? '#E0E3E7' : '#2D3843',
-      fontSize: 16,
-      width: '500px',
-      padding: '10px 12px',
-      transition: theme.transitions.create([
-        'border-color',
-        'background-color',
-        'box-shadow',
-      ]),
-      '&:focus': {
-        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-        borderColor: theme.palette.primary.main,
-      },
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: theme.palette.mode === 'light' ? '#F3F6F9' : '#1A2027',
+        border: '1px solid',
+        borderColor: theme.palette.mode === 'light' ? '#E0E3E7' : '#2D3843',
+        fontSize: 16,
+        width: '500px',
+        padding: '10px 12px',
+        transition: theme.transitions.create([
+            'border-color',
+            'background-color',
+            'box-shadow',
+        ]),
+        '&:focus': {
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+            borderColor: theme.palette.primary.main,
+        },
     },
-  }));
+}));
 
 
-  const columns = [
+const columns = [
     { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    { id: 'source', label: 'Source', minWidth: 100 },
     {
-      id: 'population',
-      label: 'Population',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
+        id: 'skills',
+        label: 'Skills',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
     },
     {
-      id: 'size',
-      label: 'Size\u00a0(km\u00b2)',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
+        id: 'languages',
+        label: 'Languages',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
     },
     {
-      id: 'density',
-      label: 'Density',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toFixed(2),
+        id: 'tools',
+        label: 'Tools',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toFixed(2),
     },
-  ];
+    {
+        id: 'gold',
+        label: 'Gold',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toFixed(2),
+    },
+];
 
 
 function createData(name, code, population, size) {
     const density = population / size;
     return { name, code, population, size, density };
-  }
-  
-  const rows = [
+}
+
+const rows = [
     createData('India', 'IN', 1324171354, 3287263),
     createData('China', 'CN', 1403500365, 9596961),
     createData('Italy', 'IT', 60483973, 301340),
@@ -190,22 +200,22 @@ function createData(name, code, population, size) {
     createData('Russia', 'RU', 146793744, 17098246),
     createData('Nigeria', 'NG', 200962417, 923768),
     createData('Brazil', 'BR', 210147125, 8515767),
-  ];
+];
 
 
 export default function Backgrounds({ mode, theme, colorMode }) {
     const [open, setOpen] = React.useState(false);
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  
+
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
-      };
-    
-      const handleChangeRowsPerPage = (event) => {
+    };
+
+    const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
-      };
+    };
 
 
     const handleDrawerClose = () => {
@@ -230,6 +240,12 @@ export default function Backgrounds({ mode, theme, colorMode }) {
 
 
     const navigate = useNavigate();
+
+    // Function to handle navigation when clicking on a Name link
+    const handleNameClick = (name) => {
+        // Replace '/destination' with the actual destination URL
+        navigate(`/destination/${name}`);
+    };
 
     return (
         <Box>
@@ -353,56 +369,80 @@ export default function Backgrounds({ mode, theme, colorMode }) {
                     </Drawer>
                 </div>
 
+                <Container maxWidth="lg" sx={{ marginTop: '100px' }} >
 
- <Paper sx={{marginTop: '200px', width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
 
+                    <Stack direction="row" alignItems="center" justifyContent="space-between" mb={10}>
+                        <Typography variant="h4" gutterBottom>
+                            Backgrounds
+                        </Typography>
+                    </Stack>
+                    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                        <TableContainer sx={{ maxHeight: 440 }}>
+                            <Table stickyHeader aria-label="sticky table">
+                                <TableHead>
+                                    <TableRow>
+                                        {columns.map((column) => (
+                                            <TableCell
+                                                key={column.id}
+                                                align={column.align}
+                                                style={{ minWidth: column.minWidth }}
+                                            >
+                                                {column.label}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((row) => {
+                                            return (
+                                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                                    {columns.map((column) => {
+                                                        const value = row[column.id];
+                                                        return (
+                                                            <TableCell key={column.id} align={column.align}>
+                                                                {/* {column.format && typeof value === 'number'
+                                                                        ? column.format(value)
+                                                                        : value} */}
+                                                                {/* Render Name as a link */}
+                                                                {column.id === 'name' ? (
+                                                                    <Link
+                                                                        to={`/destination/${value}`} // Specify the target URL based on the name
+                                                                        onClick={() => handleNameClick(value)}
+                                                                    >
+                                                                        {value}
+                                                                    </Link>
+                                                                ) : (
+                                                                    // Render other columns as plain text
+                                                                    column.format && typeof value === "number" ? (
+                                                                        column.format(value)
+                                                                    ) : (
+                                                                        value
+                                                                    )
+                                                                )}
+                                                            </TableCell>
+                                                        );
+                                                    })}
+                                                </TableRow>
+                                            );
+                                        })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            rowsPerPageOptions={[10, 25, 100]}
+                            component="div"
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </Paper>
+
+                </Container>
 
             </ThemeProvider>
         </Box>
