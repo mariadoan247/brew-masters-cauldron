2ec7f90745ba22925c88a9ec2c7750d13ae7552a
mariadoan247 <mariadoan247@ou.edu>
2023-10-11 15:02:30 -0500

    add code to find if an email already exists

--- backend/validation/signUp.js
+++ backend/validation/signUp.js
@@ -1,7 +1,34 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
+const keys = require("../config/keys");
 
-module.exports = function validateRegisterInput(data) {
+const axios = require("axios");
+
+const apiKey = keys.apiKey;
+
+async function getUserByEmail(email) {
+  try {
+    const response = await axios.post('https://us-east-2.aws.data.mongodb-api.com/app/data-iudir/endpoint/data/v1/action/findOne', {
+      collection: "users",
+      database: "5e-compendium",
+      dataSource: "brewmasters-cauldron",
+      filter: {
+        email: email
+      }
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': apiKey
+      }
+    });
+    return response.data; // this will return the found user or null if not found
+  } catch (error) {
+      console.error("Error fetching user by email:", error);
+      return null;
+  }
+}
+
+module.exports = async function validateRegisterInput(data) {
   let errors = {};
 
   // Convert empty fields to an empty string so we can use validator functions
@@ -19,6 +46,9 @@ module.exports = function validateRegisterInput(data) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  } else {
+    const existingUser = await getUserByEmail(data.email);
+    if (existingUser && existingUser.document) errors.email = "Email already exists";
   }
 
   // Password checks
