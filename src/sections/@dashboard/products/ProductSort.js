03ddaebd3f2c54d404f8b0a7216355d6f86dc1c5
neyugntina <tinaisatd@gmail.com>
2023-10-16 01:08:01 -0500

    initial commit for classes

--- src/sections/@dashboard/products/ProductSort.js
+++ src/sections/@dashboard/products/ProductSort.js
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 // @mui
-import { Menu, Button, MenuItem, Typography } from '@mui/material';
+import { Menu, Button, MenuItem, Typography } from "@mui/material";
 // component
-import Iconify from '../../../components/iconify';
+import Iconify from "../../../components/iconify";
 
 // ----------------------------------------------------------------------
 
-const SORT_BY_OPTIONS = [
-  { value: 'featured', label: 'Featured' },
-  { value: 'newest', label: 'Newest' },
-  { value: 'priceDesc', label: 'Price: High-Low' },
-  { value: 'priceAsc', label: 'Price: Low-High' },
+export const SORT_BY_OPTIONS = [
+  { value: "A-D", label: "A-D" },
+  { value: "E-H", label: "E-H" },
+  { value: "I-L", label: "I-L" },
+  { value: "M-P", label: "M-P" },
+  { value: "Q-T", label: "Q-T" },
+  { value: "U-Z", label: "U-Z" },
 ];
 
 export default function ShopProductSort() {
@@ -30,11 +32,19 @@ export default function ShopProductSort() {
         color="inherit"
         disableRipple
         onClick={handleOpen}
-        endIcon={<Iconify icon={open ? 'eva:chevron-up-fill' : 'eva:chevron-down-fill'} />}
+        endIcon={
+          <Iconify
+            icon={open ? "eva:chevron-up-fill" : "eva:chevron-down-fill"}
+          />
+        }
       >
         Sort By:&nbsp;
-        <Typography component="span" variant="subtitle2" sx={{ color: 'text.secondary' }}>
-          Newest
+        <Typography
+          component="span"
+          variant="subtitle2"
+          sx={{ color: "text.secondary" }}
+        >
+          A-D
         </Typography>
       </Button>
       <Menu
@@ -42,15 +52,15 @@ export default function ShopProductSort() {
         anchorEl={open}
         open={Boolean(open)}
         onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
       >
         {SORT_BY_OPTIONS.map((option) => (
           <MenuItem
             key={option.value}
-            selected={option.value === 'newest'}
+            selected={option.value === "newest"}
             onClick={handleClose}
-            sx={{ typography: 'body2' }}
+            sx={{ typography: "body2" }}
           >
             {option.label}
           </MenuItem>
