746705daeb4e7a661b7ec45f554a2dab4570605a
mariadoan247 <mariadoan247@ou.edu>
2023-10-16 17:32:34 -0500

    added validation logic to check email and password from the user trying to login

--- backend/validation/signIn.js
+++ backend/validation/signIn.js
@@ -1,8 +1,11 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
+const bcrypt = require("bcryptjs");
+const { getUserByEmail } = require("./getUserByEmail");
 
-module.exports = function validateLoginInput(data) {
+module.exports = async function validateLoginInput(data) {
   let errors = {};
+  let user = null;
 
   // Convert empty fields to an empty string so we can use validator functions
   data.email = !isEmpty(data.email) ? data.email : "";
@@ -13,14 +16,28 @@ module.exports = function validateLoginInput(data) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  } else {
+    const existingUser = await getUserByEmail(data.email);
+    if (!(existingUser && existingUser.document)) {
+      errors.email = "Email is not registered";
+    } else {
+      // Check password
+      const isMatch = await bcrypt.compare(data.password, existingUser.document.password);
+      if (!isMatch) {
+        errors.password = "Password incorrect";
+      } else {
+        user = existingUser; // If the password is correct, set the user object
+      }
+    }
   }
-  // Password checks
+
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
 
   return {
     errors,
-    isValid: isEmpty(errors)
+    isValid: isEmpty(errors),
+    user: user  
   };
 };
