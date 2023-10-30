702aa4a6c91acb09a11e539ae8d29caa7605a98a
neyugntina <tinaisatd@gmail.com>
2023-10-30 16:30:53 -0500

    NavBar pulled out

---
+++ src/components/pages/backgrounds.component.js
@@ -0,0 +1,224 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { Grid, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import NavBar from "../navbar";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const columns = [
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "source", label: "Source", minWidth: 100 },
+  {
+    id: "skills",
+    label: "Skills",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "languages",
+    label: "Languages",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "tools",
+    label: "Tools",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "gold",
+    label: "Gold",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData("India", "IN", 1324171354, 3287263),
+  createData("China", "CN", 1403500365, 9596961),
+  createData("Italy", "IT", 60483973, 301340),
+  createData("United States", "US", 327167434, 9833520),
+  createData("Canada", "CA", 37602103, 9984670),
+  createData("Australia", "AU", 25475400, 7692024),
+  createData("Germany", "DE", 83019200, 357578),
+  createData("Ireland", "IE", 4857000, 70273),
+  createData("Mexico", "MX", 126577691, 1972550),
+  createData("Japan", "JP", 126317000, 377973),
+  createData("France", "FR", 67022000, 640679),
+  createData("United Kingdom", "GB", 67545757, 242495),
+  createData("Russia", "RU", 146793744, 17098246),
+  createData("Nigeria", "NG", 200962417, 923768),
+  createData("Brazil", "BR", 210147125, 8515767),
+];
+
+export default function Backgrounds({ mode, theme, colorMode }) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <NavBar mode={mode} theme={theme} colorMode={colorMode}>
+      <Container
+        maxWidth="xl"
+        sx={{
+          marginTop: "140px",
+          marginLeft: "50px",
+          minHeight: "150vh", // Set a minimum height for the container
+          display: "flex", // Make it a flex container
+          flexDirection: "column", // Stack children vertically
+          width: "95%",
+        }}
+      >
+        <Grid container spacing={10} alignItems="center">
+          <Grid sx={{ display: { sm: "none", xs: "block" } }} item></Grid>
+        </Grid>
+
+        <AppBar
+          component="div"
+          color="primary"
+          position="static"
+          elevation={0}
+          sx={{ zIndex: 0 }}
+        >
+          <Toolbar>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item xs>
+                <Typography color="inherit" variant="h5" component="h1">
+                  Backgrounds
+                </Typography>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <AppBar
+          component="div"
+          position="static"
+          elevation={0}
+          sx={{ zIndex: 0 }}
+        >
+          <Typography
+            color="inherit"
+            variant="body1"
+            component="p"
+            marginLeft={5}
+            sx={{ marginTop: -0.5, marginBottom: 2 }}
+          >
+            {" "}
+            description of backgrounds goes here{" "}
+          </Typography>
+        </AppBar>
+
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={row.code}
+                      >
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {/* {column.format && typeof value === 'number'
+                                                                        ? column.format(value)
+                                                                        : value} */}
+                              {/* Render Name as a link */}
+                              {column.id === "name" ? (
+                                <Link to={`/backgrounds/${row.name}`}>
+                                  {value}
+                                </Link>
+                              ) : column.format && typeof value === "number" ? (
+                                column.format(value)
+                              ) : (
+                                value
+                              )}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Container>
+    </NavBar>
+  );
+}
---
+++ src/components/pages/feats.component.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import NavBar from "../navbar";
+
+export default function Feats({ mode, theme, colorMode }) {
+  return <NavBar mode={mode} theme={theme} colorMode={colorMode} />;
+}
