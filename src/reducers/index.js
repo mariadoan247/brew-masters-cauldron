5b0c4c0e85ed077ab3a860636589dbd30e507234
mariadoan247 <mariadoan247@ou.edu>
2023-11-01 20:11:14 -0500

    combing the notes reducer as well

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import notesReducer from "./notesReducer";
 
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  notes: notesReducer
 });
