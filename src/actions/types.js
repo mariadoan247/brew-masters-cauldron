f7c762130c395f8fa1c0245de4b65d13c63549f5
mariadoan247 <mariadoan247@ou.edu>
2023-11-15 19:46:07 -0600

    add set_races type

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,4 @@ export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const SET_USER_NOTES = "SET_USER_NOTES";
+export const SET_RACES = "SET_RACES";
