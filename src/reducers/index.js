0d1a9fa23300b2230a60fdd5fe1392ed5e816bf0
mariadoan247 <mariadoan247@ou.edu>
2023-11-15 19:46:44 -0600

    add races reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import notesReducer from "./notesReducer";
+import raceReducer from "./raceReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  notes: notesReducer
+  notes: notesReducer,
+  races: raceReducer
 });
