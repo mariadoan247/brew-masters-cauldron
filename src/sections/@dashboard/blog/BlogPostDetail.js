0c4c09d0cb63e3d3c4c629dbe0752c3431b2f777
mariadoan247 <mariadoan247@ou.edu>
2023-11-27 17:50:03 -0600

    Now displays race specific info

--- src/sections/@dashboard/blog/BlogPostDetail.js
+++ src/sections/@dashboard/blog/BlogPostDetail.js
@@ -1,22 +1,26 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import { Grid, Container } from '@mui/material';
+import {
+    Box,
+    Container,
+    Typography,
+    Grid,
+    List,
+    ListItem,
+    ListItemText,
+    Chip,
+    Divider,
+} from '@mui/material';
 import MainFeaturedPost from './MainFeaturedPost';
-import Main from './Main';
-import Sidebar from './Sidebar';
 import NavBar from "../../../components/navbar";
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
-export default function ({ mode, theme, colorMode }) {
+export default function BlogPostDetail({ mode, theme, colorMode }) {
     const { postId } = useParams();
     const races = useSelector((state) => state.races.races);
     const raceDetails = races.find((race) => race._id === postId);
-
-    console.log(raceDetails);
 
     if (!raceDetails) {
         return <div>Race details not found</div>;
@@ -24,51 +28,45 @@ export default function ({ mode, theme, colorMode }) {
 
     const mainFeaturedPost = {
         title: raceDetails.name,
-        description: "size: " + raceDetails.size + "\nspeed: " + raceDetails.speed,
+        description: "Size: " + raceDetails.size + " | Speed: " + raceDetails.speed + " | Ability: " + raceDetails.ability,
         image: raceDetails.image,
         imageText: 'main image description',
     };
 
-    const sidebar = {
-        title: 'About',
-        description:
-            'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-        archives: [
-            { title: 'March 2020', url: '#' },
-            { title: 'February 2020', url: '#' },
-            { title: 'January 2020', url: '#' },
-            { title: 'November 1999', url: '#' },
-            { title: 'October 1999', url: '#' },
-            { title: 'September 1999', url: '#' },
-            { title: 'August 1999', url: '#' },
-            { title: 'July 1999', url: '#' },
-            { title: 'June 1999', url: '#' },
-            { title: 'May 1999', url: '#' },
-            { title: 'April 1999', url: '#' },
-        ],
-
-    };
-
-    const posts = [post1, post2, post3];
+    // Helper function to render traits
+    const renderTrait = (trait) => (
+        <ListItem key={trait.name}>
+            <ListItemText primary={trait.name} secondary={trait.text.join(' ')} />
+        </ListItem>
+    );
 
     return (
         <NavBar mode={mode} theme={theme} colorMode={colorMode}>
-            <Box>
-                <Container maxWidth="lg" sx={{ marginTop: '100px' }} >
-                    <main>
-                        <MainFeaturedPost post={mainFeaturedPost} />
-                        <Grid container spacing={5} sx={{ mt: 3 }}>
-                            <Main title="From the firehose" posts={posts} />
-                            <Sidebar
-                                title={sidebar.title}
-                                description={sidebar.description}
-                                archives={sidebar.archives}
-                            />
-                        </Grid>
-                    </main>
-                </Container>
-            </Box>
+            <Container maxWidth="lg" sx={{ my: 5 }}>
+                <MainFeaturedPost post={mainFeaturedPost} />
+                <Box sx={{ my: 3 }}>
+                    <Typography variant="h5" gutterBottom>Details</Typography>
+                    <Divider sx={{ mb: 2 }} />
+                    <Typography variant="subtitle1" gutterBottom>
+                        Homebrew: {raceDetails.isHomebrew ? <CheckCircleOutlineIcon color="success" /> : <HighlightOffIcon color="error" />}  |  
+                        Public: {raceDetails.public ? <CheckCircleOutlineIcon color="success" /> : <HighlightOffIcon color="error" />}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        Spell Ability: {raceDetails.spellAbility || "No spell ability for this race"}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        Proficiency: {raceDetails.proficiency || "No proficiencies for this race"}
+                    </Typography>
+                </Box>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Typography variant="h6" gutterBottom>Traits</Typography>
+                        <List>
+                            {raceDetails.trait.map(renderTrait)}
+                        </List>
+                    </Grid>
+                </Grid>
+            </Container>
         </NavBar>
     );
-
 }
